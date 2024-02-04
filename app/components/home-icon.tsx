3518701b46f80ad03da0ec1b9661535adbdba55c
Thomas Wade <w1220w@126.com>
2024-02-04 09:14:15 +0800

    feat: 首页链接

--- app/components/home-icon.tsx
+++ app/components/home-icon.tsx
@@ -1,8 +1,8 @@
 'use client'
 import { ArrowRightEndOnRectangleIcon, HomeModernIcon } from "@heroicons/react/24/solid";
 
-export default function HomeIcon () {
+export default function HomeIcon (props?: {url?: string}) {
   return (
-    <ArrowRightEndOnRectangleIcon onClick={() => window.open('/', '_blank')} className="w-6  mr-1" />
+    <ArrowRightEndOnRectangleIcon onClick={() => window.open(props?.url ?? '/', '_blank')} className="w-6  mr-1 hover:-translate-y-1 transition duration-150 ease-out hover:ease-in"  />
   )
 }
--- app/server-component/template.tsx
+++ app/server-component/template.tsx
@@ -19,7 +19,11 @@ export default function Template(props: IProps) {
         <h1 className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
           {`5 minute${props.type ? ` ${props.type}` : ''} timer`}
         </h1>
-        {!props.isHomePage && <span className="flex items-center cursor-pointer"><HomeIcon /> <a className="whitespace-nowrap" href="/" target="_blank" title="back to home page">5 minute timer</a></span>}
+        {!props.isHomePage 
+          ? <span className="flex items-center cursor-pointer"><HomeIcon /> <a className="whitespace-nowrap hover:text-sky-500" href="/" target="_blank" title="back to home page">5 minute timer</a></span> 
+        :  <div className='fixed z-50 right-4 top-8 flex items-center cursor-pointer'>
+              <HomeIcon url="/5-minute-meditation-timer" /><a className="hover:text-sky-500" href="/5-minute-meditation-timer" target="_blank" title="go to 5 minute meditation timer" >5 minute meditation timer</a>
+            </div>}
       </div>
 
       <div className="relative w-[365px] flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px]">
