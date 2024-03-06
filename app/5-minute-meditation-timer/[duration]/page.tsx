15bd3f2d9a895bea5d5458d4adaa0bdf619bbcef
wangwei <w1220w@126.com>
2024-03-06 15:04:27 +0800

    feat: dynamic meta

--- app/5-minute-meditation-timer/[duration]/page.tsx
+++ app/5-minute-meditation-timer/[duration]/page.tsx
@@ -1,16 +1,38 @@
 import dayjs from "dayjs";
 import duration from 'dayjs/plugin/duration'
 
-import { Metadata } from "next";
+import { Metadata, ResolvingMetadata } from 'next'
 import { createRef, useRef } from "react";
 import Template from "../../server-component/template";
 import { DURATIONS } from "@/app/const";
 dayjs.extend(duration);
 
-export const metadata: Metadata = {
+/* export const metadata: Metadata = {
   title: '5 Minute Meditation Timer - Stay Focused and Relaxed',
   description: 'Discover the simplest way to enhance your meditation practice with our 5 Minute Meditation Timer. Perfect for quick mindfulness sessions, our timer helps you stay focused, manage your time efficiently, and find peace in your busy day. Try it now and transform your meditation experience.',
+} */
+ 
+type Props = {
+  params: { duration: string }
+  searchParams: { [key: string]: string | string[] | undefined }
 }
+ 
+export async function generateMetadata(
+  { params, searchParams }: Props,
+  parent: ResolvingMetadata
+): Promise<Metadata> {
+  // read route params
+  const duration = params.duration || `5`
+ 
+  return {
+    title: `${duration} Minute Meditation Timer`,
+    description: `Discover the simplest way to enhance your meditation practice with our ${duration} Minute Meditation Timer. 
+    Perfect for quick mindfulness sessions, our timer helps you stay focused, manage your time efficiently, 
+    and find peace in your busy day. Try it now and transform your meditation experience.`,
+
+  }
+}
+ 
 export const dynamicParams = false
 export async function generateStaticParams() {
  
--- app/[duration]/page.tsx
+++ app/[duration]/page.tsx
@@ -1,9 +1,38 @@
 import Image from "next/image";
 import { useEffect } from "react";
+import { Metadata, ResolvingMetadata } from 'next'
+
 import dayjs from "dayjs";
 import duration from 'dayjs/plugin/duration'
 import Template from "../server-component/template";
 dayjs.extend(duration);
+type Props = {
+  params: { duration: string }
+  searchParams: { [key: string]: string | string[] | undefined }
+}
+export async function generateMetadata(
+  { params, searchParams }: Props,
+  parent: ResolvingMetadata
+): Promise<Metadata> {
+  /* 
+    export const metadata: Metadata = {
+  title: "5 minute timer - Boost Your Productivity",
+  description: "Discover efficient ways to use a 5 minute timer for boosting productivity 
+  and focus. From Pomodoro techniques to quick breaks and meditation, 
+  learn how to incorporate short intervals into your daily routine for improved efficiency.",
+};
+  */
+  // read route params
+  const duration = params.duration || `5`
+ 
+  return {
+    title: `${duration} Minute Timer - Boost Your Productivity`,
+    description: `Discover efficient ways to use a ${duration} minute timer for boosting productivity 
+    and focus. From Pomodoro techniques to quick breaks and meditation, 
+    learn how to incorporate short intervals into your daily routine for improved efficiency.`,
+
+  }
+}
 
 export default function DurationPage({params}: {params: {duration: string}}) {
   console.log(params, `.....params....`)
