9b891b23c20c67f1a108fcf7ced946b4d9d27236
Thomas Wade <w1220w@126.com>
2024-03-09 22:17:46 +0800

    feat: sitemap change to 2

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,25 +1,23 @@
 import { MetadataRoute } from 'next'
 import { DURATIONS } from './const'
- 
+const generate = (url: string, num?: string): MetadataRoute.Sitemap[number] => {
+  return {
+    'url': url + (num === undefined ? '' : `/${num}`),
+    lastModified: new Date(),
+    changeFrequency: 'monthly',
+    priority: 0.8,
+  }
+}
 export default function sitemap(): MetadataRoute.Sitemap {
-  const toGenerate = DURATIONS?.slice(3)
-  
-  const arr: MetadataRoute.Sitemap = toGenerate?.map(num => {
-    return {
-      'url': `https://www.5minutetimer.app/${num}`,
-      lastModified: new Date(),
-      changeFrequency: 'monthly',
-      priority: 0.8,
-    }
-  })
-  const arr1: MetadataRoute.Sitemap = toGenerate?.map(num => {
-    return {
-      'url': `https://www.5minutetimer.app/5-minute-meditation-timer/${num}`,
-      lastModified: new Date(),
-      changeFrequency: 'monthly',
-      priority: 0.8,
-    }
-  })
+  const toGenerate = DURATIONS?.slice(0, 2)
+  const basePath = `https://www.5minutetimer.app`;
+  const meditationPath = `https://www.5minutetimer.app/5-minute-meditation-timer`;
+  const arr: MetadataRoute.Sitemap = [generate(basePath)].concat(toGenerate?.map(num => {
+    return generate(basePath, num)
+  }))
+  const arr1: MetadataRoute.Sitemap = [generate(meditationPath)].concat(toGenerate?.map(num => {
+    return generate(meditationPath, num)
+  }))
   return [
     ...arr,
     ...arr1
