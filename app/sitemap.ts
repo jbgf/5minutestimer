5b41d3ad4adc8f35abd7b1f151e94b1bca9c1789
Thomas Wade <w1220w@126.com>
2024-03-16 23:03:57 +0800

    feat: sitemap

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -4,12 +4,12 @@ const generate = (url: string, duration?: string): MetadataRoute.Sitemap[number]
   return {
     'url': url + (duration === undefined ? '' : `/${(duration)}`),
     lastModified: new Date(),
-    changeFrequency: 'monthly',
+    changeFrequency: 'daily',
     priority: 0.8,
   }
 }
 export default function sitemap(): MetadataRoute.Sitemap {
-  const toGenerate = DURATIONS?.slice(0, 2)
+  const toGenerate = DURATIONS?.slice(0, 3)
   const basePath = `https://www.5minutetimer.app`;
   const meditationPath = `https://www.5minutetimer.app/5-minute-meditation-timer`;
   const arr: MetadataRoute.Sitemap = [generate(basePath)].concat(toGenerate?.map(duration => {
