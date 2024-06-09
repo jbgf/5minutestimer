2e67e9a379dbb330e4d749e0633e4199766681a9
Thomas Wade <w1220w@126.com>
2024-06-09 15:33:52 +0800

    feat: 7 minutes

--- app/const.ts
+++ app/const.ts
@@ -9,8 +9,8 @@ export const MUTE_KEY = 'MUTE_TEST'
 /** 播放结束音 */
 export const END_SOUND_PLAY_KEY = 'END_SOUND_PLAY_KEY'
 
-export const DURATIONS = Array.apply(null, Array(11)).map(function (x, i) { 
-  return addPathSuffix((i + 1) * 5 + ''); 
+export const DURATIONS = Array.apply(null, Array(59)).map(function (x, index) { 
+  return addPathSuffix((index + 1) + ''); 
 })
 
 export enum TimerTypes {
